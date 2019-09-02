48d6eed811212e4647ac7c1fc5b876b34a005239
Yoon Wai Yan <waiyanyoon@gmail.com>
2019-09-02 15:53:52 +0800

    Handle and upload iOS photos to S3 bucket

--- handler.js
+++ handler.js
@@ -1,18 +1,48 @@
-'use strict';
+import request from "request-promise-native";
+import AWS from "aws-sdk";
+import crypto from "crypto";
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+import config from "./config.json";
+
+const s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+  region: config.region,
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey
+});
+
+export const main = async (event, context) => {
+  const body = JSON.parse(event.body);
+
+  const fileKey = crypto.randomBytes(3 * 4).toString("base64");
+  const fullPath = `${config.path}/${body.albumName}/${fileKey}.heic`;
+
+  const options = {
+    uri: body.url,
+    encoding: null
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  const result = await uploadToS3(options, fullPath);
+  console.log("result", result);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: result
+    })
+  };
+};
+
+const uploadToS3 = async (options, key) => {
+  const loadResult = await request(options);
+
+  const uploadResult = await s3
+    .upload({
+      Bucket: config.bucket,
+      Key: key,
+      Body: loadResult
+    })
+    .promise();
+
+  return uploadResult;
 };
